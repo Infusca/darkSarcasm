246a75d2605013d11225b801de40cc0b514a0aee
Infusca <39245332+Infusca@users.noreply.github.com>
2023-08-12 23:05:43 -0400

    initial upload

---
+++ darkSarcasm.js
@@ -0,0 +1,19 @@
+// homePage = document.querySelector('#home-page');
+// poemPage = document.querySelector('#poem-page');
+// lyricsPage = document.querySelector('#lyrics-page');
+// storiesPage = document.querySelector('#stories-page');
+// artPage = document.querySelector('#art-page');
+// submitPage = document.querySelector('#submit-page');
+
+nav = document.querySelectorAll('#nav');
+
+homeLink = nav[0].childNodes[1];
+submitLink = nav[0].childNodes[3];
+poemLink = nav[0].childNodes[5];
+storiesLink = nav[0].childNodes[7];
+lyricsLink = nav[0].childNodes[9];
+artLink = nav[0].childNodes[11];
+
+
+// console.log(poemPage);
+// console.log(poemLink);
---
+++ submission.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <?php echo $_POST['submission']; ?> <br>
+        <?php echo $_POST['author']; ?>
+    </body>
+</html>
